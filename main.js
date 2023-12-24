92e96ad103c1d3fe655227fb767f07e50cab41e9
dorrit <radithamzah12@gmail.com>
2023-12-24 16:00:59 +0700

    masi errorrr

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-// const endPoint = "http://restapi.test/api/posts"
-// const endPointLogin = "http://restapi.test/api/login";
+const endPoint = "http://restapi.test/api/posts"
+const endPoint2 = "http://restapi.test/api/post"
+const endPointLogin = "http://restapi.test/api/login";
+const endPointLogout = "http://restapi.test/api/logout";
+const token = "25|rc87yHqAYRaNXm2EkwLbcdJeDQMp1scGPx47hiQlcf017c90";
 // const isi = document.querySelector('tbody')
 // // fetch(endpoint)
 // //   .then((response) => response.json())
@@ -9,7 +12,7 @@
 
 // async function hitAPI(){
 //     try{
-//         const api = await fetch(endPoint)
+//         const api = await fetch(endPoint)s
 //         const {data} = await api.json()
 //         // console.log(data)
 //         loadPostData(data)
@@ -103,17 +106,18 @@
 
 
 
-// const getData = async () => {
-//     try {
-//         const fetchh = await fetch("http://restapi.test/api/posts")
-//         const datas = await fetchh.json()
-//         console.log(datas)
-//     } catch (err) {
-//         console.log(err)
-//     }
-// }
 
-// getData()
+
+const getData = async () => {
+    try {
+        const fetchh = await fetch("http://restapi.test/api/posts")
+        const datas = await fetchh.json()
+        console.log(datas)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
 
 
 const login = async () => {
@@ -122,19 +126,69 @@ const login = async () => {
       password: "password",
     };
     try {
-        const response = await fetch("http://restapi.test/api/login",{
-          method: 'POST',
-          header: {
-            Accept: "application/json, */*",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(dataLogin),
-        });
+      const requestOption = {
+        method: "POST",
+        headers: {
+          Accept: "application/json, */*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataLogin),
+      };
+        const response = await fetch(endPointLogin, requestOption);
         const token = await response.json()
-        console.log(token) 
+        console.log(token)
     } catch (err){
         console.log(err)
     }
 }
 
-login()
+
+const logout = async () => {
+  try{
+  $logout = await fetch(endPointLogout, {
+    method:"POST",
+    headers:{
+      'Content-Type':'application/json',
+      'Authorization':`Bearer ${token}`
+    }
+  });
+} catch (err){
+  console.log(err)
+}
+}
+
+
+// const create = async () => {
+  const form = document.querySelector('.myForm')
+  const title = document.querySelector('#title')
+  const newsContent = document.querySelector('#newsContent')
+  const image = document.querySelector('#image')
+
+
+
+  form.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    try{
+    const request = await fetch(endPoint2, {
+      method: "POST",
+      headers:{
+        Accept: "application/json, */*",
+        "Content-Type" : "application/json",
+        "Authorization" : `Bearer ${token}`
+      },
+      body:JSON.stringify({
+        title: `${title.value}`,
+        image: `${image.value}`,
+        newsContent: `${newsContent.value}`,
+      })
+    });
+    const response = await request.json();
+    console.log(response)
+    } catch (err) {
+      console.log(err)
+    }
+  });
+
+
+// login()
+// create()
